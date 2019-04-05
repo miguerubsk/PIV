534c38e102c1e496f266513ae1040590c203c5f5
miguerubsk <miguerubsk@gmail.com>
2019-04-05 17:58:08 +0200

    Update p6_1.py

--- p6_1.py
+++ p6_1.py
@@ -51,6 +51,11 @@ def calcularUmbralGeneral(img):
 def calcularUmbralOtsu(histograma):
 	val_max = -999
 	umbral = -1
+	q1 = 0
+	q2 = 0
+	m1 = 0
+	m2 = 0
+	val = 0
 	for t in range(1,255):
 	    q1 = np.sum(histograma[:t])
 	    q2 = np.sum(histograma[t:])
@@ -92,33 +97,33 @@ if __name__ == '__main__':
 		menu()
 		opcionMenu = input("Elija una opción: ")
 		if opcionMenu=="1":
-			print("Umbralizando la imagen... \nEspera#")
 			#Carga de la imagen con la que se va a trabjar
-			Nombre = "triangulo.png" #input("Introduzca el nombre de la imagen con extension: ")
+			Nombre = input("Introduzca el nombre de la imagen con extension: ")
 			img = cv2.imread(Nombre, cv2.IMREAD_GRAYSCALE)
 			#Se mustra la imagen original
 			#r = cv2.imshow(Nombre, img)
 			#Se umbraliza con el método general
+			print("Umbralizando la imagen... \nEspera")
 			result = umbralizar(img, calcularUmbralGeneral(img))
 			#Se muestra la imagen resultante y se graba en un fichero
 			#p = cv2.imshow(Nombre+"_umbralizacion", result)
-			cv2.imwrite("p3_umbralizacion_general.png",result)
-			print("Terminado, comprueba la imagen resultante: p3_umbralizacion_general.png")
+			cv2.imwrite(Nombre+"_umbralizacion_general.png",result)
+			print("Terminado, comprueba la imagen resultante: "+Nombre+"_umbralizacion_general.png")
 			input("Pulsa una tecla para continuar")
 			cv2.destroyAllWindows()
 		elif opcionMenu=="2":
-			print("Umbralizando la imagen... \nEspera")
 			#Carga de la imagen con la que se va a trabjar
-			Nombre = "triangulo.png" #input("Introduzca el nombre de la imagen con extension: ")
+			Nombre = input("Introduzca el nombre de la imagen con extension: ")
 			img = cv2.imread(Nombre, cv2.IMREAD_GRAYSCALE)
 			#Se mustra la imagen original
 			#r = cv2.imshow(Nombre, img)
 			#Se umbraliza la imagen con el método Otsu
+			print("Umbralizando la imagen... \nEspera")
 			result = umbralizar(img, calcularUmbralOtsu(calculoHistograma(img)))
 			#Se muestra la imagen resultante y se graba en un fichero
 			#p = cv2.imshow(Nombre+"_umbralizacion_otsu", result)
-			cv2.imwrite("p3_umbralizacion_otsu.png",result)
-			print("Terminado, comprueba la imagen resultante: p3_umbralizacion_otsu.png")
+			cv2.imwrite(Nombre+"_umbralizacion_otsu.png",result)
+			print("Terminado, comprueba la imagen resultante:"+Nombre+" _umbralizacion_otsu.png")
 			input("Pulsa una tecla para continuar")
 			cv2.destroyAllWindows()
 		elif opcionMenu=="3":
