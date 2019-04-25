3b9923d14ea2f441841d3455cf83ccee5b60c9a9
miguerubsk <miguerubsk@gmail.com>
2019-04-25 10:59:04 +0200

    parametros ajustados

--- p7/p7.py
+++ p7/p7.py
@@ -21,19 +21,26 @@ if __name__ == '__main__':
 	os.system('clear')
 	detectorCaras = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 	detectorBocas = cv2.CascadeClassifier("mouth.xml")
+	detectorOjos = cv2.CascadeClassifier("haarcascade_eye.xml")
+	detectorNariz = cv2.CascadeClassifier("nose.xml")
 
 
-	Nombre = "6" #input("Introduzca el nombre de la imagen con extension: ")
+	Nombre = "4" #input("Introduzca el nombre de la imagen con extension: ")
 	imgGris = cv2.imread(Nombre+".jpg", cv2.IMREAD_GRAYSCALE)
 	img = cv2.imread(Nombre+".jpg")
 
 
-	caras = detectorCaras.detectMultiScale(imgGris, 1.3, 4)
-	bocas = detectorBocas.detectMultiScale(imgGris, 1.22, 10)
+	caras = detectorCaras.detectMultiScale(imgGris, 1.3, 5)
+	bocas = detectorBocas.detectMultiScale(imgGris, 1.8, 5)
+	ojos  = detectorOjos.detectMultiScale(imgGris, 1.1, 20)
+	nariz = detectorNariz.detectMultiScale(imgGris, 1.2, 5)
+
 
 
 	img = pintarRectangulo(img, caras, 0, 0, 255 )
 	img = pintarRectangulo(img, bocas, 255, 255, 0)
+	img = pintarRectangulo(img, ojos, 0,255,0 )
+	img = pintarRectangulo(img, nariz, 255, 0, 0 )
 
 
 	cv2.imshow(Nombre+"_caras_sonrisas",img)
